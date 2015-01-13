8d6916d170ffe13f75f266374ab31be86d78bf4a
Aditya Shridhar Hegde <adityahegderocks@gmail.com>
2015-01-13 09:10:51 +0530

    Alpha V0.1.0

--- src/js/belongsTo.js
+++ src/js/belongsTo.js
@@ -12,6 +12,9 @@ define([
  * Optionally can create the instance with mixin. A single mixin can be passed or a map of mixins as 4th parameter with key extracted from object using mixinKey (5th parameter) can be passed.
  * 'defaultMixin' (6th parameter) is used when object[mixinKey] is not present.
  *
+ * Can pass a 7th argument. A registry of objects. If the object (identified by an idKey) is already present, reference to the object is returned.
+ * 8th argument is the idKey
+ *
  * @method belongsTo
  * @for Utils
  * @static
@@ -21,13 +24,17 @@ define([
  * @param {Mixin|Object} [mixin]
  * @param {String} [mixinKey]
  * @param {String} [defaultMixin]
+ * @param {Object} [registry]
+ * @param {String} [idKey]
  * @returns {Instance}
  */
-function belongsTo(modelClass, modelClassKey, defaultKey, mixin, mixinKey, defaultMixin) {
+function belongsTo(modelClass, modelClassKey, defaultKey, mixin, mixinKey, defaultMixin, registry, idKey) {
   modelClass = modelClass || Ember.Object;
-  var hasInheritance = Ember.typeOf(modelClass) !== "class",
-      hasMixin = mixin instanceof Ember.Mixin,
-      hasMixinInheritance = !hasMixin && Ember.typeOf(mixin) === "object";
+  var
+  hasInheritance = Ember.typeOf(modelClass) !== "class",
+  hasMixin = mixin instanceof Ember.Mixin,
+  hasMixinInheritance = !hasMixin && Ember.typeOf(mixin) === "object",
+  hasRegistry = registry && idKey;
   return Ember.computed(function(key, newval) {
     if(Ember.typeOf(modelClass) === 'string') {
       modelClass = Ember.get(modelClass);
@@ -38,21 +45,30 @@ function belongsTo(modelClass, modelClassKey, defaultKey, mixin, mixinKey, defau
       hasMixin = mixin instanceof Ember.Mixin;
       hasMixinInheritance = !hasMixin && Ember.typeOf(mixin) === "object";
     }
+    if(Ember.typeOf(registry) === 'string') {
+      registry = Ember.get(registry);
+      hasRegistry = registry && idKey;
+    }
     if(arguments.length > 1) {
       if(newval) {
         var classObj = modelClass;
-        if(hasInheritance) classObj = modelClass[Ember.isEmpty(newval[modelClassKey]) ? defaultKey : newval[modelClassKey]];
-        if(!(newval instanceof classObj)) {
-          if(hasMixin) {
-            newval = classObj.createWithMixins(mixin, newval);
+        if(hasRegistry && registry[newval[idKey]]) {
+          newval = registry[newval[idKey]];
+        }
+        else {
+          if(hasInheritance) classObj = modelClass[Ember.isEmpty(newval[modelClassKey]) ? defaultKey : newval[modelClassKey]];
+          if(!(newval instanceof classObj)) {
+            if(hasMixin) {
+              newval = classObj.createWithMixins(mixin, newval);
+            }
+            else if(hasMixinInheritance) {
+              newval = classObj.createWithMixins(mixin[newval[mixinKey] || defaultMixin], newval);
+            }
+            else {
+              newval = classObj.create(newval);
+            }
+            newval.set("parentObj", this);
           }
-          else if(hasMixinInheritance) {
-            newval = classObj.createWithMixins(mixin[newval[mixinKey] || defaultMixin], newval);
-          }
-          else {
-            newval = classObj.create(newval);
-          }
-          newval.set("parentObj", this);
         }
       }
       return newval;
