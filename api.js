334d96e66f42a3e4f90fa141085e462f92d4982c
Aditya Hegde <adityahegderocks@gmail.com>
2015-06-02 20:09:12 -0700

    Adding yuidocs

---
+++ api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "EmberUtilsCore",
+        "EmberUtilsCore.ObjectWithArrayMixin"
+    ],
+    "modules": [
+        "ember-utils-core"
+    ],
+    "allModules": [
+        {
+            "displayName": "ember-utils-core",
+            "name": "ember-utils-core"
+        }
+    ]
+} };
+});
